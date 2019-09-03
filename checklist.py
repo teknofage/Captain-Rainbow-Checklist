ef6e69c91c59dd7b151ea4023d97c8a1e2582d2e
teknofage <teknofage@gmail.com>
2019-09-03 00:12:18 -0700

    init rep

--- checklist.py
+++ checklist.py
@@ -1,118 +1,102 @@
-# print("Hello World") #prints what's in the brackets
-
-# checklist = list() #creates a list object in memory that we can refer back to by the name checklist
-# # checklist
-
-# # def my_fun_function(say_this): # defines the function
-#     # print(say_this) # tells the function what it does to the data
-# # my_fun_function('Hello World') # gives the data inside the function
-
-# #CREATE
-# checklist = list() #creates a new empty list object and puts it in memory.
-# checklist.append('Blue') #runs a chunk of code within that object and passes in the value we wish to add to our list.
-# # print(checklist) #prints the entire list to the console.
-# checklist.append('Orange') # runs the same append code and passes 'Blue' to our list.
-# # print(checklist) # prints the entire list again.
-
-# # def create(item): #defines a variable with an item/value inside
-# #     checklist.append(item) #appends the variable to checklist.
-# # print(checklist[0]) #prints item 0 in the list
-
-# # READ
-# def read(index): #defines a read function allows terminal to read what's in the list
-#     item = checklist[index] #return the value that lives at that index in our checklist and save it in a memory location we named item
-#     return item #returns the data it has read from the checklist in the place called item
-
-# # READ in fewer lines
-# def read(index):
-#     return checklist[index]
-
-# # UPDATE
-# checklist = ['Blue', 'Orange']
-# checklist[1] = 'Cats' #overwriting the data located in the second position of our list with the string Cats.
-# # print(checklist)
-
-# # UPDATE
-# def update(index, item): #make a function out of this.
-#     checklist[index] = item
-#     #So that takes care of the first three letters, C, R, and U. Now we just the the last one - Destroy.
-
-#     #Enter the pop method. Calling the pop method in our list will remove the last item in the list by default. If we want a different item we must specify it.
-# checklist = ['Blue', 'Cats']
-# checklist.pop(1) #You can see that we've removed the second element in our list.
-# print(checklist)
-
-# DESTROY
-# def destroy(index): #turns destroy via pop into a function
-    # checklist.pop(index) #points the function at checklist/location
-
-# Create our Checklist
+# print('Hello World')
 checklist = list()
 
-# CREATE
+checklist.append('Blue')
+print(checklist)
+
+checklist.append('Orange')
+print(checklist)
+print(checklist)
+
+checklist = ['Blue', 'Cats']
+checklist.pop(1)
+print(checklist)
+
+
+def my_fun_function(say_this):
+	print(say_this)
+	my_fun_function('Hello World')
+
+
 def create(item):
-    # Create item code here
-
-# READ
-    def read(index):
-# Read code here
-
-# UPDATE
-    def update(index, item):
-# Update code here
-
-# DESTROY
-    def destroy(index):
-    # Destroy code here
-                
-    def list_all_items():
-# Code to list all items in list
-                    
-    def test(): #create a function that will house all of our testing code. 
-    # Add your testing code here
-                    
-    test()
-                    
-   def test():
-#     #days of the week
-#     create(days of the week)
-#     create("Monday")
-#     create("Tuesday")
-#     create("Wednesday")
-#     create("Thursday")
-#     create("Friday")
-#     create("Saturday")
-#     create("Sunday")
-    
-#     #colours
-#create(list of colors)
-#     create("red")
-#     create("orange")
-#     create("yellow")
-#     create("green")
-#     create("blue")
-#     create("violet")
-#     create("indigo")
-
-        # #items of clothing
-        #create(items of  clothing)
-        # create("left shoe")
-        # create ("right shoe")
-        # create("left sock")
-        # create ("right sock")
-        # create("trousers")
-        # create ("shirt")
-        # create ("jacket")
-
-# for (days of the week + items of clothing);
-#     color
+	checklist.append(item)
+	checklist[0]
 
 
-  #for each day + each item of clothing, choose a color that is not the same as another color chosen that day
+def read(index):
+	print(checklist[index])
+	return checklist[index]
 
-  #for each item 
-    #for each day
-     #for each item of clothing
-       # print item of clothing specific to day
 
-print("Hello World")
+def update(index, item):
+	checklist[index] = item
+
+
+def destroy(index):
+	checklist.pop(index)
+
+
+def list_all_items():
+	index = 0
+	for list_item in checklist:
+		print("{} {}".format(index, list_item))
+		index += 1
+
+
+def select(function_code):
+   	# Create item
+	if function_code == "C":
+		input_item = user_input("Input item:")
+		create(input_item)
+
+   	# Read item
+	elif function_code == "R":
+		item_index = int(user_input("Index Number?"))
+
+		# Remember that item_index must actually exist or our program will crash.
+		read(item_index)
+
+   	# Print all items
+	elif function_code == "P":
+		list_all_items()
+  
+	elif function_code == "Q":
+		return False
+
+	# Catch all
+	else: 
+	 	print("Unknown Option")
+   
+	return True  
+
+	 
+	
+	
+def user_input(prompt):
+	user_input = input(prompt)
+	return user_input
+
+
+def test():
+	create("purple sox")
+	create("red cloak")
+	print(read(0))
+	print(read(1))
+	update(0, "purple socks")
+	destroy(1)
+	print(read(0))
+	print(read(1))
+	list_all_items()
+	select("C")
+	list_all_items()
+	select("R")
+	list_all_items()
+	user_value = user_input("Please Enter a value:")
+	print(user_value)
+ 
+test()
+
+running = True
+while running:
+	selection = user_input("Press C to add to list, R to Read from list and P to display list")
+	running = select(selection)
